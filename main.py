494ae2aa13d36cdb119c5938fecb6f97ec11393c
brightbeam <68129223+brightbeamgames@users.noreply.github.com>
2020-08-15 20:59:02 -0400

    update main add img folder

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 600
 FPS = 30
 
 WHITE = (255, 255, 255)
@@ -11,6 +11,20 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+
+class Player(pygame.sprite.Sprite):
+    #sprite for the Player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
 # Initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
@@ -19,6 +33,8 @@ pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # Game Loop
 running = True
 while running:
